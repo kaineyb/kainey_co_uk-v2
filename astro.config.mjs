c83283543b72bc93287492116db0e3d3ea109612
Kaine Bruce <kaineyb@gmail.com>
2022-11-28 14:30:27 +0000

    added react, mdx and styled components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx()]
+});
