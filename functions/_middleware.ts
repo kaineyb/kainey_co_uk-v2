83a9e9c81833f4e34f6c5226dc5b91ee6b5694a1
Kaine Bruce <kaineyb@gmail.com>
2023-01-20 11:10:43 +0000

    update: email addresses

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,9 +2,9 @@ import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
 export const onRequest: PagesFunction = mailChannelsPlugin({
   personalizations: [
-    { to: [{ name: "ACME Support", email: "info@kainey.co.uk" }] },
+    { to: [{ name: "Kaine Bruce", email: "kaineyb@gmail.com" }] },
   ],
-  from: { name: "ACME Support", email: "info@kainey.co.uk" },
+  from: { name: "Contact Form @ Kainey.co.uk", email: "contact@kainey.co.uk" },
   respondWith: () => {
     return new Response(
       `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
