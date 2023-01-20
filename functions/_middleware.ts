2ea1678ba4d728a927190e9fd893db9afd73b82d
Kaine Bruce <kaineyb@gmail.com>
2023-01-20 13:07:29 +0000

    update: redirect

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,13 +1,13 @@
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-const baseURl = "https://dev.kainey.pages.dev";
-
 export const onRequest: PagesFunction = mailChannelsPlugin({
   personalizations: [
     { to: [{ name: "Kaine Bruce", email: "kaineyb@gmail.com" }] },
   ],
   from: { name: "Contact Form @ Kainey.co.uk", email: "contact@kainey.co.uk" },
-  respondWith: () => {
-    return Response.redirect(baseURl + "/contact/success");
-  },
+  respondWith: () =>
+    new Response(null, {
+      status: 302,
+      headers: { Location: "/success" },
+    }),
 });
