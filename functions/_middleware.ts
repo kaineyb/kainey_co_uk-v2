5d9a7fea2302a973821f97a4d910cf42f2073deb
Kaine Bruce <kaineyb@gmail.com>
2023-01-20 12:09:25 +0000

    updated: added baseURL Env

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,11 +1,13 @@
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
+const baseURl = process.env.CF_PAGES_URL;
+
 export const onRequest: PagesFunction = mailChannelsPlugin({
   personalizations: [
     { to: [{ name: "Kaine Bruce", email: "kaineyb@gmail.com" }] },
   ],
   from: { name: "Contact Form @ Kainey.co.uk", email: "contact@kainey.co.uk" },
   respondWith: () => {
-    return Response.redirect("/contact/success");
+    return Response.redirect(baseURl + "/contact/success");
   },
 });
