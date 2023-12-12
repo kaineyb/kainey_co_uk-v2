db83f96d4f0cbe69cfbcf3f2637dcd81ee542d18
Kaine Bruce <kaineyb@gmail.com>
2023-12-12 22:51:05 +0000

    added timezone page

---
+++ src/components/TimeTest.vue
@@ -0,0 +1,27 @@
+<template>
+  Local Time:
+  <br />
+  {{ currentTime }}
+  <br />
+  Time Zone:
+  {{ tz }}
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const browserData = Intl.DateTimeFormat().resolvedOptions();
+
+const locale = browserData.locale;
+const tz = browserData.timeZone;
+
+const currentTime = ref(getCurrentTime());
+
+function getCurrentTime() {
+  return new Date().toLocaleString(locale);
+}
+
+setInterval(() => {
+  currentTime.value = getCurrentTime();
+}, 100);
+</script>
+<style scoped></style>
