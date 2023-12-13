56183984d494b676f1453e5138473b92d343a300
Kaine Bruce <kaineyb@gmail.com>
2023-12-13 08:38:39 +0000

    test

--- src/components/TimeTest.vue
+++ src/components/TimeTest.vue
@@ -1,27 +1,29 @@
 <template>
-  Local Time:
-  <br />
-  {{ currentTime }}
-  <br />
-  Time Zone:
-  {{ tz }}
+  <div class="test">
+    Local Time:
+    {{ currentTime }}
+    <br />
+    Time Zone:
+    {{ browserData.timeZone }}
+  </div>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
 
 const browserData = Intl.DateTimeFormat().resolvedOptions();
 
-const locale = browserData.locale;
-const tz = browserData.timeZone;
-
 const currentTime = ref(getCurrentTime());
 
 function getCurrentTime() {
-  return new Date().toLocaleString(locale);
+  return new Date().toLocaleString(browserData.locale);
 }
 
 setInterval(() => {
   currentTime.value = getCurrentTime();
 }, 100);
 </script>
-<style scoped></style>
+<style scoped>
+.test {
+  outline: 1px solid gold;
+}
+</style>
